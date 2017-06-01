08ba74d89e29bedac4f1a90a45a08f790b59d2fe
Jessica Grinberg <jessgrinberg@gmail.com>
2017-06-01 13:53:45 -0700

    accordion toggle plus/minus

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 $('#myTab').tabCollapse();
+
+var selectIds = $('#collapseOne,#collapseTwo,#collapseThree');
+$(function ($) {
+    selectIds.on('show.bs.collapse hidden.bs.collapse', function () {
+        $(this).prev().find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus');
+    })
+});
