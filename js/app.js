5f94f973ba7ce4e26967b902844906bdfff56a79
Jessica Grinberg <jessgrinberg@gmail.com>
2017-06-01 17:35:06 -0700

    modified cta bottom logic and fade div on scrolling

--- js/app.js
+++ js/app.js
@@ -6,3 +6,13 @@ $(function ($) {
         $(this).prev().find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus');
     })
 });
+
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 300) {
+        $('.cta-bottom').fadeIn();
+    } else {
+        $('.cta-bottom').fadeOut();
+    }
+
+});
